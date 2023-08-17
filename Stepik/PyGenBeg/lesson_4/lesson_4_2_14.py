0e1458a293e9e86b7e637698590d9116d31a095e
Anas Davletshin <anas.davletshin@gmail.com>
2023-08-17 11:04:55 +0300

    Added lesson_4_2_8, lesson_4_2_15.

---
+++ Stepik/PyGenBeg/lesson_4/lesson_4_2_14.py
@@ -0,0 +1,9 @@
+c1 = int(input())
+l1 = int(input())
+c2 = int(input())
+l2 = int(input())
+
+if c1 == c2 or l1 == l2:
+    print('YES')
+else:
+    print('NO')
---
+++ Stepik/PyGenBeg/lesson_4/lesson_4_2_15.py
@@ -0,0 +1,9 @@
+c1 = int(input())
+l1 = int(input())
+c2 = int(input())
+l2 = int(input())
+
+if c2 - 1 <= c1 <= c2 + 1 and l2 - 1 <= l1 <= l2 + 1:
+    print('YES')
+else:
+    print('NO')
