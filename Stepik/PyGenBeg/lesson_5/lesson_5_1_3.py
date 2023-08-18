858a400d376f499f6a1e42bae0671ba71a4d0573
Anas Davletshin <anas.davletshin@gmail.com>
2023-08-18 16:28:50 +0300

    Added lesson_5_1_1-lesson_5_1_8.

---
+++ Stepik/PyGenBeg/lesson_5/lesson_5_1_3.py
@@ -0,0 +1,7 @@
+age = int(input())
+gender = input()
+
+if gender == 'f' and 10 <= age <= 15:
+    print('YES')
+else:
+    print('NO')
---
+++ Stepik/PyGenBeg/lesson_5/lesson_5_1_5.py
@@ -0,0 +1,11 @@
+num = int(input())
+
+if num % 2 == 1:
+    print('YES')
+else:
+    if 2 <= num <= 5:
+        print('NO')
+    elif 6 <= num <= 20:
+        print('YES')
+    else:
+        print('NO')
